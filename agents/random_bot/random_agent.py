3ecae6cbbcd4dce3a4131f301407852d95024382
Minh T. Nguyen <mnguyen0226@vt.edu>
2021-07-07 16:33:38 -0400

    minor change

--- agents/random_bot/random_agent.py
+++ agents/random_bot/random_agent.py
@@ -4,6 +4,8 @@ from poke_env.player.utils import cross_evaluate
 from tabulate import tabulate
 
 async def main():
+    """Create a 3 random pokemon trainer bot to to fight against each other with random actions
+    """
     # We create three random players
     players = [
         RandomPlayer(max_concurrent_battles=10) for _ in range(3)
@@ -15,8 +17,8 @@ async def main():
     # Defines a header for displaying results
     table = [["-"] + [p.username for p in players]]
 
-    # Adds one line per player with corresponding results
-    for p_1, results in cross_evaluation.items():
+    # Adds one line per player with corresponding results - loop just for create a table
+    for p_1, results in cross_evaluation.items(): 
         table.append([p_1] + [cross_evaluation[p_1][p_2] for p_2 in results])
 
     # Displays results in a nicely formatted table.
